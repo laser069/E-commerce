c57e7403d744f686d7b0c8b871161bcd90868034
Sachin-2006 <sachin06kaizen@gmail.com>
2025-01-31 09:54:11 +0530

    :)

---
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,21 @@
+import Login from "../../components/Login/login";
+
+import React from "react";
+
+
+const LoginPage = () =>{
+    <div>
+
+        <Login/>
+    </div>
+}
+
+export default LoginPage
+
+
+
+
+
+
+
+

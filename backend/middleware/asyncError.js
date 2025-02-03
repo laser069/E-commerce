f4cfcb082815922094af53592dd912e6ac3fda0c
Sachin-2006 <sachin06kaizen@gmail.com>
2025-02-03 18:09:05 +0530

    error

---
+++ backend/middleware/asyncError.js
@@ -0,0 +1,7 @@
+
+module.exports=(theFunct)=>(req,res,next)=>
+    {
+        Promise.resolve(theFunct(req,res,next)).catch(next)
+    }
+    
+    
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,22 @@
-const app = require("./app")
-const ConnectDB = require('./db/db')
+const app = require('./app')
+const connectDB=require('./db/db')
 
-process.on('uncaughtException',(err)=>{
-    console.log(`Error:${err.message}`)
-    process.exit(0);    
+process.on('uncaughtException',(error)=>{
+    console.log(`server shutthing down ${error.message}`)
+    process.exit(1)
 })
 
-
-if(process.env.NODE_ENV !== "PRODUCTION"){
-    require('dotenv').config({path:'config/.env'})
-}
-
-
-ConnectDB()
+ if(process.env.NODE_ENV !== 'PRODUCTION'){
+     require('dotenv').config({path:'config/.env'})}
+ 
+connectDB()
 
 
 
 app.listen(process.env.PORT,()=>{
-    console.log("Success!!")
+    console.log(`Server suceessfully listen at port http://localhost:${process.env.PORT}`)
 })
+
+            
+
+process.on('unhandledRejection',()=>{process.exit(1)})

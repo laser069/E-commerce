d1dd25d587bef9cc8638c335cf5395769f1480aa
Sachin-2006 <sachin06kaizen@gmail.com>
2025-03-06 10:33:10 +0530

    Milestone-15

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const ErrorHandler = require('./middleware/error')
 const cors = require('cors')
 const user = require("./Control/user")
 const app = express();
+const path = require('path')
+const product = require("./Control/Product")
 
 app.use(cors())
 app.use(express.json())
@@ -10,9 +12,12 @@ app.use(express.urlencoded({extended:true}))
 app.use("/",express.static('uploads'))
 
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/products', express.static(path.join(__dirname, 'products')));
 require('dotenv').config({path:'backend/config/.env'})
 
 app.use('/api/v2',user)
+app.use('/api/product',product)
 app.use(ErrorHandler)
 
 module.exports = app;
